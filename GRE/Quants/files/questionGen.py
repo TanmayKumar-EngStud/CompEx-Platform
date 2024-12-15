0de29bfc94204dee901203146dda9ac1bf579502
Tanmay kumar <tanmay.kumar2019@vitstudent.ac.in>
2024-12-15 13:15:07 +0530

    Generate Assistants are working fine

---
+++ GRE/Quants/files/questionGen.py
@@ -0,0 +1,25 @@
+import os
+import json
+from dotenv import load_dotenv
+from langchain_experimental.openai_assistant import OpenAIAssistantRunnable
+load_dotenv()
+
+
+assistant_id = json.load(open(os.path.join(os.path.dirname(__file__), "../../assistant_ids.json"), "r"))["QuestionGen"]
+
+
+class QuestionGen:
+    def __init__(self):
+        self.llm = OpenAIAssistantRunnable(
+            model="gpt-4o-mini",
+            api_key=os.getenv("OPENAI_API_KEY"),
+            assistant_id=assistant_id
+        )
+
+    def generate_question(self, input_data):
+        response = self.llm.invoke({"content":input_data})
+        return [message.content[0].text.value for message in response][0]
+
+questionGen = QuestionGen()
+
+print(questionGen.generate_question("<[Percentages]> - <2> - <Word Problem>"))
--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-
-load_dotenv()
-
-llm = ChatOpenAI(model_name="gpt-4o-mini")
-
-print(llm.invoke("What is the capital of France?").content)
