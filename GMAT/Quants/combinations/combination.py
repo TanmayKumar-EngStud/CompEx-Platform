7cb67813c6576738c925e55b5363f1740ec58ff2
Tanmay kumar <tanmay.kumar2019@vitstudent.ac.in>
2024-12-31 18:04:26 +0530

    added combination for GMAT Quants

---
+++ GMAT/Quants/combinations/combination.py
@@ -0,0 +1,64 @@
+import json
+import math
+import os
+
+class Combination:
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), "combination.json"), "r") as file:
+            self.combination = json.load(file)
+        self.combination_number = self.combination["combination number"]
+    
+    def pair_combination(self, length, combination_number):
+        idx_1 = math.floor((combination_number - 1) / (length - 1))
+        idx_2 = math.floor((combination_number - 1) % (length - 1))
+        if idx_2 >= idx_1:
+            idx_2 += 1
+        return idx_1, idx_2 % length
+    
+    def generate_combination(self):
+        prompt = []
+        for section_name, details in self.combination.items():
+            if section_name != "combination number":
+                string = f"{section_name}"
+                initiator = 1
+                question_style_selected = ""
+                filtered_topics = []
+
+                for key, values in details.items():
+                    if key == "questionStyle":
+                        length = len(values)
+                        idx = math.floor(self.combination_number / initiator) % length
+                        question_style_selected = values[idx]
+                        string += (f" - <{values[idx]}>").replace("*", "").replace("$", "")
+                        initiator *= length
+
+                    elif key == "questionTopic":
+                        if "Geometric Properties" in question_style_selected:
+                            filtered_topics = [topic for topic in values if '*' in topic]
+                        elif "Data Interpretation" in question_style_selected:
+                            filtered_topics = [topic for topic in values if '$' in topic]
+                        else:
+                            filtered_topics = values
+                        
+                        length = len(filtered_topics)
+                        idx = math.floor(self.combination_number / initiator) % length
+                        string += (f" - <{filtered_topics[idx]}>").replace("*", "").replace("$", "")
+                        initiator *= length
+
+                    elif key == "tableType" and "Data Interpretation" in question_style_selected:
+                        length = len(values)
+                        idx = math.floor(self.combination_number / initiator) % length
+                        string += (f" - <{values[idx]}>").replace("*", "").replace("$", "")
+                        initiator *= length
+
+                    elif key == "questionTheme" and "Word Problems" in question_style_selected:
+                        idx_1, idx_2 = self.pair_combination(len(values), self.combination_number)
+                        string += (f" - <{values[idx_1]}, {values[idx_2]}>").replace("*", "").replace("$", "")
+
+                prompt.append(string)
+
+        self.combination_number += 1
+        self.combination["combination number"] = self.combination_number
+        with open(os.path.join(os.path.dirname(__file__), "combination.json"), "w") as file:
+            json.dump(self.combination, file, indent=4)
+        return prompt
--- GMAT/Quants/main.py
+++ GMAT/Quants/main.py
@@ -5,43 +5,48 @@ from langchain_experimental.openai_assistant import OpenAIAssistantRunnable
 from files.simpleQuestionGeneration import SimpleQuestionGeneration
 from files.parentChildQuestionGeneration import ParentChildQuestionGeneration
 from files.dataSufficiencyQuestionGeneration import DataSufficiencyQuestionGeneration
+from combinations.combination import Combination
+
 load_dotenv()
+class GMATQuants:
+   def __init__(self):
+      combination = Combination()
+      self.assistant_id_simple_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Simple-Questions"]
+      self.assistant_id_data_sufficiency_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Data-Sufficiency-Questions"]
+      self.assistant_id_parent_child_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Parent-Child-Questions"]
+      self.prompt = combination.generate_question()
+      print(self.prompt)
+   def generate_question(self):
+      
+      if ("data sufficiency" in self.prompt.lower()):
+         llm = OpenAIAssistantRunnable(
+            model="gpt-4o-mini",
+            api_key=os.getenv("OPENAI_API_KEY"),
+            assistant_id=self.assistant_id_data_sufficiency_questions
+         )
+         dataSufficiencyQuestionGeneration = DataSufficiencyQuestionGeneration(llm, self.prompt)
+         
+         questionData = dataSufficiencyQuestionGeneration.generate_question()
+         return questionData
+      elif ("graph" in self.prompt.lower()):
+         llm = OpenAIAssistantRunnable(
+            model="gpt-4o-mini",
+            api_key=os.getenv("OPENAI_API_KEY"),
+            assistant_id=self.assistant_id_parent_child_questions
+         )
+         parentChildQuestionGeneration = ParentChildQuestionGeneration(llm, self.prompt)
+         questionData = parentChildQuestionGeneration.generate_question()
+         return questionData
+      else:
+         llm = OpenAIAssistantRunnable(
+            model="gpt-4o-mini",
+            api_key=os.getenv("OPENAI_API_KEY"),
+            assistant_id=self.assistant_id_simple_questions
+         )
+         simpleQuestionGeneration = SimpleQuestionGeneration(llm, self.prompt)
+         
+         questionData = simpleQuestionGeneration.generate_question()
+         return questionData
 
-assistant_id_simple_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Simple-Questions"]
-assistant_id_data_sufficiency_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Data-Sufficiency-Questions"]
-assistant_id_parent_child_questions = json.load(open(os.path.join(os.path.dirname(__file__), "../assistant_ids.json"), "r"))["GMAT-Quants-Parent-Child-Questions"]
-
-prompt = "<Algebra> - <1> - <word problem> - <problem solving> - <bar graph>"
-
-if ("data sufficiency" in prompt.lower()):
-   llm = OpenAIAssistantRunnable(
-      model="gpt-4o-mini",
-      api_key=os.getenv("OPENAI_API_KEY"),
-      assistant_id=assistant_id_data_sufficiency_questions
-   )
-   dataSufficiencyQuestionGeneration = DataSufficiencyQuestionGeneration(llm, prompt)
-   
-   questionData = dataSufficiencyQuestionGeneration.generate_question()
-   print("Data Sufficiency Question Generation")
-   print(json.dumps(questionData, indent=4))
-elif ("graph" in prompt.lower()):
-   llm = OpenAIAssistantRunnable(
-      model="gpt-4o-mini",
-      api_key=os.getenv("OPENAI_API_KEY"),
-      assistant_id=assistant_id_parent_child_questions
-   )
-   parentChildQuestionGeneration = ParentChildQuestionGeneration(llm, prompt)
-   questionData = parentChildQuestionGeneration.generate_question()
-   print("Parent Child Question Generation")
-   print(json.dumps(questionData, indent=4)) 
-else:
-   llm = OpenAIAssistantRunnable(
-      model="gpt-4o-mini",
-      api_key=os.getenv("OPENAI_API_KEY"),
-      assistant_id=assistant_id_simple_questions
-   )
-   simpleQuestionGeneration = SimpleQuestionGeneration(llm, prompt)
-   
-   questionData = simpleQuestionGeneration.generate_question()
-   print("Simple Question Generation")
-   print(json.dumps(questionData, indent=4))
+gmatQuants = GMATQuants()
+print(gmatQuants.generate_question())
